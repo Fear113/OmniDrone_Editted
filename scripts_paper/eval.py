1a65c70bed1fe3632be7fb4ef1f1dec5b6cd8741
sunwoolee0504 <sunwoolee@unist.ac.kr>
2024-08-17 21:45:17 +0900

    Update eval.py

--- scripts_paper/eval.py
+++ scripts_paper/eval.py
@@ -14,6 +14,20 @@ from omni_drones.learning import (
     TDMPCPolicy,
 )
 
+# modify - 1
+class Every:
+    def __init__(self, func, steps):
+        self.func = func
+        self.steps = steps
+        self.i = 0
+
+    def __call__(self, *args, **kwargs):
+        if self.i % self.steps == 0:
+            self.func(*args, **kwargs)
+        self.i += 1
+import imageio
+# modify - 1
+
 algos = {
     "mappo": MAPPOPolicy,
     "happo": HAPPOPolicy,
@@ -47,17 +61,60 @@ def main(cfg):
     if formation_checkpoint is not None:
         formation_policy.load_state_dict(torch.load(formation_checkpoint))
 
+# modify - 2
+    def record_frame(frames, *args, **kwargs):
+            frame = env.render(mode="rgb_array")
+            frames.append(frame)
+
+    frames_transport = []
+    frames_formation = []
+    seed = 1
+
+    env.enable_render(True)
+    env.eval()
+    env.set_seed(seed)
     state = env.reset()
-    while True:
-        while not state['done']:
-            state = env.step(transport_policy(state, deterministic=True))['next']
 
+    print("start formation")
+
+    while not state['done']:
+        state = env.step(formation_policy(state, deterministic=True))['next']
+        record_frame(frames_formation)
+
+    print("finish formation")
+
+    with torch.no_grad():
         state_snapshot = env.snapshot_state()
-        simulation_app.context.close_stage()
-        simulation_app.context.new_stage()
-        env = env_class(cfg, headless=cfg.headless, initial_state=state_snapshot)
-        state = env.reset()
 
+    simulation_app.context.close_stage()
+    simulation_app.context.new_stage()
+
+    env = env_class(cfg, headless=cfg.headless, initial_state=state_snapshot)  
+    state = env.reset()
+
+    print("start transport")
+
+    while not state['done']:
+        state = env.step(transport_policy(state, deterministic=True))['next']
+        record_frame(frames_transport)
+
+    print("finish transport")
+
+    if len(frames_formation):
+        imageio.mimsave("result_video/formation_video.mp4", frames_formation, fps=0.5 / cfg.sim.dt)
+        print("completed the formation video")
+    if len(frames_transport):
+        imageio.mimsave("result_video/transport_video.mp4", frames_transport, fps=0.5 / cfg.sim.dt)
+        print("completed the transport video")
+    
+    frames_total = frames_formation + frames_formation
+
+    if len(frames_total):
+        imageio.mimsave("result_video/total_video.mp4", frames_total, fps=0.5 / cfg.sim.dt)
+        print("completed the total video")
+
+    print("done everything")
+# modify - 2
 
 if __name__ == "__main__":
     main()
