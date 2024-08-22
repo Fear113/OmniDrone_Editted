efb671ff163a681d238e2eb22e5d6a0ead1354aa
mlic <isaac1018@unist.ac.kr>
2024-08-23 05:55:29 +0900

    multi group logistics

--- omni_drones/envs/logistics/state_snapshot.py
+++ omni_drones/envs/logistics/state_snapshot.py
@@ -11,6 +11,7 @@ class Payload:
 class ConnectedPayload(Payload):
     payload_pos: torch.Tensor
     payload_rot: torch.Tensor
+    payload_vel: torch.Tensor
     joint_pos: torch.Tensor
     joint_vel: torch.Tensor
 
@@ -20,14 +21,18 @@ class DisconnectedPayload(Payload):
     payload_rot: torch.Tensor
 
 @dataclass
-class Group:
+class GroupSnapshot:
     drone_pos: torch.Tensor
     drone_rot: torch.Tensor
     drone_vel: torch.Tensor
     target_payload_idx: Optional[int]
     is_transporting: bool
+    count: int
     payloads: list[ConnectedPayload | DisconnectedPayload]
+
+    def target_payload(self):
+        return self.payloads[self.target_payload_idx]
 
 @dataclass
 class StateSnapshot:
-    groups: list[Group]
+    group_snapshots: list[GroupSnapshot]
